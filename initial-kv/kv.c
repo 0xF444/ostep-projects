1a54480ce9ee8a07239b50e8edcf0236e1279cd8
nozom2 <nozom0093@gmail.com>
2026-09-23 14:04:24 +0300

    add todo

--- initial-kv/kv.c
+++ initial-kv/kv.c
@@ -13,6 +13,7 @@ int main(int argc, char **argv)
     }
     else
     {
+        // TODO: Think of a way to return the same data with the length of commands parsed
         Command_t *commands = ParseCommands(argc, argv);
     }
 }
